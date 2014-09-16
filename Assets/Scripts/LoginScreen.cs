48f8e842c99696926d96c04e402defb189463cef
Tetherys <woaortom@gmail.com>
2014-09-16 21:03:52 +0200

    scripts gemaakt voor screenmanager en loginscreen
    alle input velden worden opgehaald in de loginscreen en de checkboxes
    swappen met elkaar zodat er altijd 1 actief is.

---
+++ Assets/Scripts/LoginScreen.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginScreen : MenuScreen {
+
+	private UIInput username;
+	private UIInput emailaddress;
+	private UIInput password;
+	private UICheckbox loginCheckBox;
+	private UICheckbox registerCheckBox;
+	
+	// Use this for initialization
+	void Start () {
+		username = GameObject.Find ("UserNameInput").GetComponent<UIInput> ();
+		emailaddress = GameObject.Find ("EmailInput").GetComponent<UIInput> ();
+		password = GameObject.Find ("PasswordInput").GetComponent<UIInput> ();
+		loginCheckBox = GameObject.Find ("LoginCheckBox").GetComponent<UICheckbox> ();
+		registerCheckBox = GameObject.Find ("RegisterCheckBox").GetComponent<UICheckbox> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void login()
+	{
+		Debug.Log (username.text + emailaddress.text);
+	}
+
+	public void OnLoginCheckBoxActivate()
+	{
+		this.CheckBoxSwapper (loginCheckBox, registerCheckBox);
+	}
+
+	public void OnRegisterCheckBoxActivate()
+	{
+		this.CheckBoxSwapper (registerCheckBox, loginCheckBox);
+	}
+
+	private void CheckBoxSwapper(UICheckbox active, UICheckbox unactive)
+	{
+		if(active != null)
+		{
+			if(active.isChecked == true)
+			{
+				if(unactive.isChecked == true)
+				{
+					unactive.isChecked = false;
+				}
+			}
+			else
+			{
+				if(unactive.isChecked == false)
+				{
+					unactive.isChecked = true;
+				}
+			}
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/MenuScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MenuScreen : MonoBehaviour {
+
+	private bool isActive = false;
+
+	public void setActive(bool isActive)
+	{
+		this.isActive = isActive;
+	}
+}
